63e1eb91aba3542c0c015cde73340ade226b630b
AnumiVithana <anumivithana1@gmail.com>
2025-07-13 18:36:36 +0530

    Service layer for custom user details

---
+++ src/main/java/com/example/inklet/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.inklet.service;
+
+import com.example.inklet.entity.User;
+import com.example.inklet.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
