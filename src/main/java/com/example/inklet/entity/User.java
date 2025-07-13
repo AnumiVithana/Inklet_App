97a577567244942e67672ff0e13a07c54d1eb019
Atheeq <mhdatheeq0@gmail.com>
2025-07-13 14:54:59 +0530

    entity tables created

---
+++ src/main/java/com/example/inklet/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.inklet.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Post> posts;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Like> likes;
+}
